d8a34b23696207f57a66b7de6b3234d918b58605
kaylynfletcher <efaquietshadow@gmail.com>
2020-06-10 22:36:48 -0600

    added random slots

--- game.py
+++ game.py
@@ -11,18 +11,32 @@
 # Show amount you won if any
 # Show current wallet and ask if they want to play again.
 
+import random
+
 print("Welcome to the _blank_ slot machine")
-print("You have __ in your wallet")
+print("You have $1000 in your wallet")
 print("Would you like to play? Y/N  ")
 play_again = input()
 
-wallet_amount = ("$")
-starting_wallet = ("$" + 1000)
+# tofix so i can have $ and a number
+wallet_amount = ()
+
+def roll_slots(): 
+    slot_options = ("| Unicorn |", "| Dragon |", "| Sword |", "| Gold |", "| Castle |", "| Knight |")
+    print(random.choice(slot_options), random.choice(slot_options), random.choice(slot_options))
+
 
 # if statement to check if input equeals Y or N
-if play_again == "Y":
+if play_again.upper() == "Y":
     print("What would you like to bet?")
     current_bet = input("$")
+    roll_slots()
 
-if play_again == "N":
+if play_again.upper() == "N":
     print("We hope to see you again")
+
+
+# The options it randomly chooses between
+
+
+# see if i can figure out how to make a table later
